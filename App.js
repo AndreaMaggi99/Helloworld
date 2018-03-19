21e70ec086f4ae617b346369d582d087c44bb55d
AndreaMaggi99 <maggiolino99@gmail.com>
2018-03-19 13:04:41 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+'use strict'
+const Hapi=require('hapi');
+const auth=require('hapi-auth-basic');
+const server=new Hapi.Server();
+var rubrica=[
+    {
+        'nome':'gallo',
+        'professione':'rompiballe'
+    }
+];
+server.connection({host:'localhost',port:3000});
+server.route({
+    method:'GET',
+    path:'/{nome}',
+    handler:function(request,reply)
+    {
+        reply('Hi '+encodeURIComponent(request.params.nome)+'!');
+    }
+});
+server.route({
+    method:'GET',
+    path:'/',
+    handler:function(request,reply)
+    {
+        reply(encodeURIComponent('Hi!'));
+    }
+});
+server.route({
+    method:'GET',
+    path:'/post',
+    handler:function(request,reply)
+    {
+        reply(JSON.stringify(rubrica));
+    }
+});
+server.route({
+    method:'POST',
+    path:'/',
+    handler:function(request,reply){
+rubrica.push({
+    nome: request.payload.nome,
+    professione:request.payload.professione
+});
+reply(JSON.stringify(rubrica));
+console.log(rubrica);
+    }
+})
+server.start(function(err){if(err)throw err; console.log("Running on " +server.info.uri)});
